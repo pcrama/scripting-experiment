7aba6fe72d75b8bf6e679ddb3cb356e4c8255fba
Philippe Crama <pcfeb0009@gmx.com>
2020-12-24 12:13:32 +0100

    Reduce unit test duration from 28 to 17.5s

--- python/ownlib/tests/test_checkout_dependencies_lib.py
+++ python/ownlib/tests/test_checkout_dependencies_lib.py
@@ -45,43 +45,81 @@ class FindByHexShaPrefixTests(unittest.TestCase):
 
 
 class PullTests(unittest.TestCase):
-    BRANCH = 'test'
-    OTHER_BRANCH = f'other{BRANCH}'
+    branches = []
+    '''List of branches in the class level local and remote repositories that test instances may use'''
+
+    OTHER_BRANCH = f'otherbranch'
     REMOTE_COMMIT = FakeCommit(
                 {'data': 'line 1\n'}, '2nd commit', 'h2', 'h1', None, None)
 
-    def setUp(self):
+    @classmethod
+    def setUpClass(cls):
+        '''Create local and remote repository only once to speed up tests'''
         common_commits = [
             FakeCommit(
-                {'data': 'one line\n'}, '1st commit', 'h1', None, None, self.BRANCH),
+                {'data': 'one line\n'}, '1st commit', 'h1', None, None, None),
             FakeCommit(
-                {'data': 'data\n'}, 'other commit', None, None, None, self.OTHER_BRANCH),
+                {'data': 'data\n'}, 'other commit', None, None, None, cls.OTHER_BRANCH),
         ]
-        remote_commits = [self.REMOTE_COMMIT]
-        self.remote_context = test_repository(common_commits)
-        self.remote = self.remote_context.__enter__()
-        self.local_context = test_repository_with_remote(self.remote)
-        self.local = self.local_context.__enter__()
-        self.local.remotes.origin.fetch()
+        remote_commits = [cls.REMOTE_COMMIT]
+        cls.remote_context = test_repository(common_commits)
+        cls.remote = cls.remote_context.__enter__()
+        # Set up branches, all pointing to commit 'h1' so that each test can
+        # pop off one branch and work from that:
+        for branch_number in range(30):
+            cls.branches.append(f'test_{branch_number:02}')
+            cls.remote.create_head(
+                cls.branches[-1], cls.remote.human_id_mapping['h1'])
+        cls.local_context = test_repository_with_remote(cls.remote)
+        cls.local = cls.local_context.__enter__()
+        cls.local.remotes.origin.fetch()
         # Now that we have fetched, the local repository has the same commits
         # available as the remote repository, so copy the mappings to the
         # local repository.
-        for human_id, hex_sha in self.remote.human_id_mapping.items():
-            self.local.human_id_mapping[human_id] = hex_sha
+        for human_id, hex_sha in cls.remote.human_id_mapping.items():
+            cls.local.human_id_mapping[human_id] = hex_sha
+        # Create new commit in remote but do not advance its branches yet,
+        # otherwise tests are going to interfere with each other.
+        for commit in remote_commits:
+            commit.create(cls.remote)
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            cls.remote_context.__exit__(None, None, None)
+        finally:
+            cls.local_context.__exit__(None, None, None)
+
+    def setUp(self):
+        # Hint: Increase the number of pre-made branches if the next statement
+        # raises an exception.
+        self.BRANCH = self.branches.pop()
         # Set up tracking of remote BRANCH.
         self.local.git.checkout(self.BRANCH)
-        for commit in remote_commits:
-            commit.create(self.remote)
-        self.remote.branches[self.BRANCH].set_commit(self.remote.human_id_mapping['h2'])
+        self.local.head.reset(index=True, working_tree=True)
+        self.needs_merge_abort = False
+        self.needs_rebase_abort = False
+        # Setup remote branch to its new commit only now to avoid the
+        # pull/fetch of prior test cases interfering:
+        self.remote.branches[self.BRANCH].set_commit(
+            self.remote.human_id_mapping['h2'])
+        # Check that the remote advanced, leaving local behind
+        assert self.local.branches[self.BRANCH].commit != \
+            self.remote.branches[self.BRANCH].commit
+        # Check that local has not even fetched
+        assert self.local.remotes.origin.refs[self.BRANCH].commit != \
+            self.remote.branches[self.BRANCH].commit
 
     def tearDown(self):
-        try:
-            self.remote_context.__exit__(None, None, None)
-        finally:
-            self.local_context.__exit__(None, None, None)
+        if self.needs_merge_abort:
+            self.local.git.merge('--abort') # clean up for future test
+        if self.needs_rebase_abort:
+            self.local.git.rebase('--abort') # clean up for future test
 
     def setup_diverging_branch_without_conflict(self):
-        assert self.local.head.commit == self.local.branches[self.BRANCH].commit
+        initial_head_commit = self.local.head.commit
+        # Check we are on the right branch
+        assert initial_head_commit == self.local.branches[self.BRANCH].commit
         # This also advances the local BRANCH to point at the newly created
         # commit, effectively diverging from the remote:
         FakeCommit(
@@ -92,11 +130,13 @@ class PullTests(unittest.TestCase):
             None,
             None
         ).create(self.local)
+        assert initial_head_commit in self.local.head.commit.parents
+        assert self.local.branches[self.BRANCH].commit == self.local.head.commit
         # The local commit is unknown in the remote repository.
         assert self.local.head.commit not in self.remote.iter_commits()
         # But they both have the same parent.
         assert (self.local.head.commit.parents ==
-                self.remote.head.commit.parents)
+                self.remote.branches[self.BRANCH].commit.parents)
 
     def setup_diverging_branch_with_conflict(self):
         assert self.local.head.commit == self.local.branches[self.BRANCH].commit
@@ -119,10 +159,8 @@ class PullTests(unittest.TestCase):
         assert self.local.head.commit not in self.remote.iter_commits()
         # But they both have the same parent.
         assert (self.local.head.commit.parents ==
-                self.remote.head.commit.parents)
+                self.remote.branches[self.BRANCH].commit.parents)
 
-
-class PullFfOnlyTests(PullTests):
     def test_given_no_local_changes_but_on_other_branch_when_calling_pull_ff_only_no_error_is_raised(self):
         # Given: Check out other local branch to check that pull_ff_only sets
         # up the correct branch.
@@ -151,8 +189,6 @@ class PullFfOnlyTests(PullTests):
         # Then
         self.assertTrue('Not possible to fast-forward' in cm.exception.stderr)
 
-
-class PullRebaseTests(PullTests):
     def test_given_no_local_changes_but_on_other_branch_when_calling_pull_rebase_no_error_is_raised(self):
         # Given: Check out other local branch to check that pull_rebase sets
         # up the correct branch.
@@ -185,14 +221,13 @@ class PullRebaseTests(PullTests):
         # Given
         self.setup_diverging_branch_with_conflict()
         # When
+        self.needs_rebase_abort = True
         with self.assertRaises(git.exc.GitCommandError) as cm:
             pull_rebase(self.local, self.BRANCH)
         # Then
         self.assertTrue('conflict' in cm.exception.stderr)
         self.assertTrue('Could not apply' in cm.exception.stderr)
 
-
-class PullMergeTests(PullTests):
     def test_given_no_local_changes_but_on_other_branch_when_calling_pull_merge_no_error_is_raised(self):
         # Given: Check out other local branch to check that pull_merge sets
         # up the correct branch.
@@ -231,6 +266,7 @@ class PullMergeTests(PullTests):
         # Given
         self.setup_diverging_branch_with_conflict()
         # When
+        self.needs_merge_abort = True
         with self.assertRaises(git.exc.GitCommandError) as cm:
             pull_merge(self.local, self.BRANCH)
         # Then
