b751082136d20f686e145b23b604f669b7c5b1fb
Philippe Crama <pcfeb0009@gmx.com>
2021-02-02 22:33:53 +0100

    Improve test output

--- python/ownlib/tests/all_tests.cmd
+++ python/ownlib/tests/all_tests.cmd
@@ -9,8 +9,11 @@ SETLOCAL ENABLEEXTENSIONS
 
 CD "%~dp0..\.."
 
-REM Unit tests
+ECHO Unit tests
+ECHO ------------------------------------------------------------
 "%~dp0..\..\virtualenv\bin\python3.exe" -m unittest -v "%~dp0__init__.py"
 
-REM Doctests
+ECHO ============================================================
+ECHO Doctests
+ECHO ------------------------------------------------------------
 FOR /F %%F in ('dir /b "%~dp0..\*.py"') DO IF NOT %%F == __init__.py "%~dp0..\..\virtualenv\bin\python3.exe" -m doctest "%~dp0..\%%F"
