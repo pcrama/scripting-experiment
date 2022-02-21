58bf2064c161b9f4fe42d8ed0421b26ca903589c
Philippe Crama <pcfeb0009@gmx.com>
2022-02-21 06:36:23 +0100

    Initial commit of Fable/Elmish input form

---
+++ python/italsdf/input-form/src/App.fs
@@ -0,0 +1,400 @@
+module App
+
+open Elmish
+open Elmish.React
+open Feliz
+
+type PlateType = | InsideMenu | OutsideMenu
+
+type Plate = | Assiettes | Fondus | Bolo | Scampis | Tiramisu | Tranches
+
+let AllPlates = [Assiettes; Fondus; Bolo; Scampis; Tiramisu; Tranches]
+
+type Tickets<'t> =
+    {
+        assiettes : 't
+        fondus : 't
+        bolo : 't
+        scampis : 't
+        tiramisu : 't
+        tranches : 't
+    }
+    with member this.Get = function
+                | Assiettes -> this.assiettes
+                | Fondus -> this.fondus
+                | Bolo -> this.bolo
+                | Scampis -> this.scampis
+                | Tiramisu -> this.tiramisu
+                | Tranches -> this.tranches
+
+let PrixEntreeEuros = 9
+let PrixBoloEuros = 12
+let PrixScampisEuros = PrixBoloEuros + 5
+let PrixDessertEuros = 6
+let PrixMenuBolo = PrixEntreeEuros + PrixBoloEuros + PrixDessertEuros - 2
+let PrixMenuScampis = PrixEntreeEuros + PrixScampisEuros + PrixDessertEuros - 2
+
+let TicketsNames = {
+    assiettes = "Assiette italienne"
+    fondus = "Croquettes au fromage"
+    bolo = "Spaghetti bolognaise"
+    scampis = "Spaghetti aux scampis"
+    tiramisu = "Tiramisu maison"
+    tranches = "Tranche napolitaine"
+}
+
+type State =
+    {
+        name : string
+        email : string
+        places : int
+        menus : int
+        insideMenu : Tickets<int>
+        outsideMenu : Tickets<int>
+        nameError : string option
+        emailError : string option
+        placesErrors : string list
+        menusErrors : string list
+        insideMenuErrors : Tickets<string list>
+        outsideMenuErrors : Tickets<string list>
+    }
+
+type Msg =
+    | SetNumberOfTickets of PlateType * Plate * int
+    | SetName of string
+    | SetEmail of string
+    | SetPlaces of int
+    | SetMenus of int
+
+let updateMenu (tickets : Tickets<'a>) (plate: Plate) (newValue: 'a): Tickets<'a> =
+    match plate with
+    | Assiettes -> { tickets with assiettes = newValue }
+    | Fondus -> { tickets with fondus = newValue } 
+    | Bolo -> { tickets with bolo = newValue } 
+    | Scampis -> { tickets with scampis = newValue } 
+    | Tiramisu -> { tickets with tiramisu = newValue } 
+    | Tranches -> { tickets with tranches = newValue }
+
+let updateNoValidate (msg: Msg) (state: State): State =
+    match msg with
+    | SetNumberOfTickets (plateType, plate, newCount) ->
+        match plateType with
+        | InsideMenu -> { state with insideMenu = updateMenu state.insideMenu plate newCount }
+        | OutsideMenu -> { state with outsideMenu = updateMenu state.outsideMenu plate newCount }
+    | SetName newName -> { state with name = newName }
+    | SetEmail newEmail -> { state with email = newEmail }
+    | SetPlaces newPlaces -> { state with places = newPlaces }
+    | SetMenus newMenus -> { state with menus = newMenus }
+
+let validatePositive (count: int) (namePlural: string) (tail: string list): string list =
+    if count < 0
+    then (sprintf "Le nombre de %s doit être positif." namePlural :: tail)
+    else tail
+
+let validateStrictPositive (count: int) (namePlural: string) (tail: string list): string list =
+    if count < 1
+    then (sprintf "Le nombre de %s doit être supérieur à 0." namePlural :: tail)
+    else tail
+
+let validateInclusiveBelow (count: int) (namePlural: string) (max: int) (tail: string list): string list =
+    if count > max
+    then (sprintf "Le nombre de %s doit être inférieur ou égal à %d." namePlural max :: tail)
+    else tail
+
+let naivePlural (count: int) (singular: string): string =
+    match count with
+    | 1 -> sprintf "1 %s" singular
+    | _ -> sprintf "%d %ss" count singular
+
+let altList (f: 'a -> 'b list) (xs: 'a) (ys: 'a): 'b list =
+    match f xs with
+    | [] -> f ys
+    | result -> result
+
+let FormMaxInt = 50
+
+let validatePlatePair
+        (plate1Count: int)
+        (plate1PluralName: string)
+        (plate2Count: int)
+        (plate2PluralName: string)
+        (target: int option) =
+    let rangeErrors =
+        (validateInclusiveBelow plate1Count
+                                plate1PluralName
+                                FormMaxInt
+                                (validatePositive plate1Count plate1PluralName []),
+         validateInclusiveBelow plate2Count plate2PluralName FormMaxInt
+                                (validatePositive plate2Count plate2PluralName []))
+    let menuErrors =
+        match target with
+        | None -> ([], [])
+        | Some targetValue ->
+            match targetValue - plate1Count - plate2Count with
+            | pos when pos > 0 ->
+              let msg = sprintf "Pour %s, vous devez commander plus de %s ou de %s."
+                                (naivePlural targetValue "menu")
+                                plate1PluralName
+                                plate2PluralName
+              ([msg], [msg])
+            | neg when neg < 0 ->
+                let msg = sprintf "Pour cette commande, vous devez commander %s en plus."
+                                  (naivePlural (-neg) "menu")
+                ((if plate1Count > 0 then [msg] else []),
+                 (if plate2Count > 0 then [msg] else []))
+            | _ -> ([], [])
+    ((altList fst rangeErrors menuErrors),
+     (altList snd rangeErrors menuErrors))
+
+let validateTickets (t: Tickets<int>) (target: int option): Tickets<string list> =
+    let (assiettes, fondus) = validatePlatePair t.assiettes "Assiettes italiennes" t.fondus "Croquettes au fromage" target
+    let (bolo, scampis) = validatePlatePair t.bolo "Spaghettis bolognaise" t.scampis "Spaghettis aux scampis" target
+    let (tiramisu, tranches) = validatePlatePair t.tiramisu "Tiramisus" t.tranches "Tranches napolitaines" target
+    { assiettes = assiettes
+      fondus = fondus
+      bolo = bolo
+      scampis = scampis
+      tiramisu = tiramisu
+      tranches = tranches }
+
+type Validated<'t> =
+    { Raw : string
+      Parsed : Option<'t> }
+
+module Validated =
+    let createEmpty() : Validated<_> =
+        { Raw = ""; Parsed = None }
+
+    let success raw value : Validated<_> =
+        { Raw = raw; Parsed = Some value }
+
+    let failure raw : Validated<_> =
+        { Raw = raw; Parsed = None }
+
+    let iter f v =
+        match v.Parsed with
+        | Some x -> f x |> Some
+        | None -> None
+
+let parseValidate parse s =
+    try Validated.success s (parse s)
+    with | _ -> Validated.failure s
+
+let validateState (state: State) =
+    let menusErrors = validateInclusiveBelow state.menus
+                                             "menus"
+                                             FormMaxInt
+                                             (validatePositive state.menus "menus" [])
+    let target = match menusErrors with
+                 | [] -> Some state.menus
+                 | _ -> None
+    { state with
+          nameError = if state.name.Trim() = "" then Some "Ce champ est obligatoire." else None
+          emailError = match state.email.Trim() with
+                       | "" -> Some "Ce champ est obligatoire."
+                       | x when (let at = x.IndexOf('@') in at > -1 && x.IndexOf('.', at) - at > 1)
+                           -> None
+                       | _ -> Some "Veuillez saisir une adresse email valide."
+          placesErrors = validateInclusiveBelow state.places
+                                                "places"
+                                                FormMaxInt
+                                                (validateStrictPositive state.places "places" [])
+          insideMenuErrors = validateTickets state.insideMenu target
+          outsideMenuErrors = validateTickets state.outsideMenu None
+          menusErrors = menusErrors
+    }
+
+let update (msg: Msg) (state: State): State =
+    let newState = updateNoValidate msg state
+    validateState newState
+
+let ErrorRed = "darkRed"
+
+let ErrorBorderStyle =
+    [style.borderColor ErrorRed; style.borderStyle.solid; style.borderWidth 1]
+
+let errorDiv (len: Styles.ICssUnit option) (e: string) =
+    let width = match len with
+                | Some w -> [style.width w]
+                | None -> []
+    Html.div [
+        [style.color ErrorRed
+         style.fontStyle.oblique
+         length.percent 85 |> style.fontSize] |> List.append width |> prop.style
+        prop.text e]
+
+let inputNumber (tickets: Tickets<int>) (errors: Tickets<string list>) (idPrefix: string) (plate: Plate) (onChange: int -> unit) =
+    let id = sprintf "%s-%A" idPrefix plate
+    let value = tickets.Get plate
+    let errorsS = errors.Get plate
+    let label = TicketsNames.Get plate
+    let basicInputProps = [
+                prop.style [
+                    length.em 3 |> style.width
+                    style.textAlign.right]
+                prop.id id
+                prop.type' "number"
+                prop.value value
+                prop.onChange onChange]
+    let basicChildren p = [
+            Html.label [
+                prop.style [
+                    style.display.inlineBlock
+                    length.em 15 |> style.width]
+                prop.htmlFor id
+                prop.text label]
+            Html.input p]
+    let children = match errorsS with
+                   | [] -> basicChildren basicInputProps
+                   | es -> prop.style ErrorBorderStyle :: basicInputProps
+                           |> basicChildren
+                           |> List.append
+                           <| List.map (length.em 20 |> Some |> errorDiv) es
+    Html.li [
+        sprintf "div-just-for-%s" id |> prop.id
+        prop.children children]
+
+let renderTicket (isMenu: PlateType) (state: State) (dispatch: Msg -> unit) =
+    let setNumber p v = SetNumberOfTickets (isMenu, p, v) |> dispatch
+    let mkInput, header =
+        match isMenu with
+        | InsideMenu ->
+            (inputNumber state.insideMenu state.insideMenuErrors "inside",
+             Html.div [
+                prop.children [
+                Html.input [
+                    prop.style [
+                        length.em 3 |> style.width
+                        style.textAlign.right]
+                    prop.type' "number"
+                    prop.min 0
+                    prop.max FormMaxInt
+                    prop.onChange (fun v -> SetMenus v |> dispatch)]
+                Html.textf " menu%s" <| if state.menus = 1 then "" else "s"]])
+        | OutsideMenu ->
+            let onlyCountIfReasonable p =
+                match state.outsideMenuErrors.Get p with
+                | [] -> state.outsideMenu.Get p
+                | _ -> 0
+            (inputNumber state.outsideMenu state.outsideMenuErrors "outside",
+             naivePlural (List.sumBy onlyCountIfReasonable AllPlates)
+                         "ticket"
+             |> Html.textf "Hors menu: %s")
+    let renderedTicket =
+        [("Entrées", [Assiettes; Fondus])
+         ("Plats", [Bolo; Scampis])
+         ("Desserts", [Tiramisu; Tranches])]
+        |> (List.map <| fun (title: string, plates) ->
+               Html.li [
+                   Html.text title
+                   plates |> List.map (fun p -> setNumber p |> mkInput p) |> Html.ul])
+        |> Html.ul
+    Html.div [
+        prop.style [
+            length.ex 1 |> style.marginRight
+            style.verticalAlign.top
+            style.display.inlineBlock]
+        prop.children [header; renderedTicket]]
+
+let inputText (id: string) (type': string) (label: string) (placeholder: string) (value: string) (error: string option) (onChange: string -> unit) =
+    let basicChildren s = [
+        Html.label [
+            prop.style [length.ex 0.3 |> style.marginBottom]
+            prop.htmlFor id
+            prop.text label]
+        Html.br []
+        Html.input [
+            prop.style <| (length.percent 100 |> style.width) :: s
+            prop.id id
+            prop.type' type'
+            length.percent 100 |> prop.width
+            prop.placeholder placeholder
+            prop.value value
+            prop.onTextChange onChange]]
+    let children =
+        match error with
+        | None -> basicChildren []
+        | Some e -> List.append (basicChildren ErrorBorderStyle) [
+            errorDiv None e]
+    Html.div [
+        sprintf "div-just-for-%s" id |> prop.id
+        prop.style [length.ex 1 |> style.marginBottom]
+        prop.children children]
+
+let totalPriceInEuros (state: State): int =
+    state.insideMenu.bolo * PrixMenuBolo +
+    state.insideMenu.scampis * PrixMenuScampis +
+    (state.outsideMenu.assiettes + state.outsideMenu.fondus) * PrixEntreeEuros +
+    state.outsideMenu.bolo * PrixBoloEuros +
+    state.outsideMenu.scampis * PrixScampisEuros +
+    (state.outsideMenu.tiramisu + state.outsideMenu.tranches) * PrixDessertEuros
+
+let hasErrors (state: State): bool =
+    let isSome = function
+        | None -> false
+        | _ -> true
+    isSome state.nameError ||
+    isSome state.emailError ||
+    List.exists (fun p -> List.isEmpty (state.insideMenuErrors.Get p) = false) AllPlates ||
+    List.exists (fun p -> List.isEmpty (state.outsideMenuErrors.Get p) = false) AllPlates ||
+    List.isEmpty state.menusErrors = false
+
+let render (state: State) (dispatch: Msg -> unit) =
+    let hasErrors = hasErrors state
+    Html.div[
+        inputText "name" "text" "Nom:" "Vos places et votre commande de tickets seront à votre nom" state.name state.nameError (SetName >> dispatch)
+        inputText "email" "email" "Email:" "Nous pourrions avoir besoin de votre email pour le tracing" state.email state.emailError (SetEmail >> dispatch)
+        Html.div [
+            renderTicket InsideMenu state dispatch
+            renderTicket OutsideMenu state dispatch]
+        match (hasErrors, totalPriceInEuros state) with
+        | (true, _)
+        | (false, 0) -> Html.text ""
+        | (false, p) -> Html.textf "Prix total de votre commande: %d €." p
+        Html.div [
+            prop.style [style.overflow.hidden]
+            prop.children [
+            Html.div [
+                prop.style [length.ex 4 |> style.width; style.verticalAlign.top; style.textAlign.left; style.float'.left]
+                prop.children [
+                    Html.input [
+                        prop.id "gdpr_accepts_use"
+                        prop.name "gdpr_accepts_use"
+                        prop.type' "checkbox"
+                        prop.value true
+                    ]
+                ]
+            ]
+            Html.div [
+                prop.style [style.overflow.hidden; style.verticalAlign.top]
+                prop.children [
+                    Html.label [
+                        prop.for' "gdpr_accepts_use"
+                        prop.text "J’autorise la Société Royale d’Harmonie de Braine-l’Alleud à utiliser mon adresse email pour m’avertir de ses futures activités."
+                    ]
+                ]
+            ]]]
+        if hasErrors
+        then Html.text ""
+        else Html.input [prop.type' "submit"; prop.value "Confirmer la réservation"]]]
+
+let init() =
+    let state = {
+        name = ""
+        email = ""
+        places = 1
+        menus = 0
+        insideMenu = { assiettes = 0; fondus = 0; bolo = 0; scampis = 0; tiramisu = 0; tranches = 0 }
+        outsideMenu = { assiettes = 0; fondus = 0; bolo = 0; scampis = 0; tiramisu = 0; tranches = 0 }
+        nameError = None
+        emailError = None
+        menusErrors = []
+        placesErrors = []
+        insideMenuErrors = { assiettes = []; fondus = []; bolo = []; scampis = []; tiramisu = []; tranches = [] }
+        outsideMenuErrors = { assiettes = []; fondus = []; bolo = []; scampis = []; tiramisu = []; tranches = [] }
+    }
+    validateState state
+
+Program.mkSimple init update render
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
---
+++ python/italsdf/input-form/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path")
+
+module.exports = (env, argv) => {
+    // extract build mode from command-line
+    const mode = argv.mode;
+    const result = {
+        mode: mode,
+        entry: "./src/App.fsproj",
+        module: {
+            rules: [{
+                test: /\.fs(x|proj)?$/,
+                use: "fable-loader"
+            }]
+        }
+    }
+
+    if ("development" == mode) {
+        Object.assign(
+            result, {
+                devtool: "eval-source-map",
+
+                devServer: {
+                    devMiddleware: {
+                        publicPath: "/"
+                    },
+                    port: 8080,
+                    proxy: undefined,
+                    hot: true,
+                    static: {
+                        directory: path.resolve(__dirname, "./dist"),
+                        staticOptions: {},
+                    },
+                },
+            });
+    }
+
+    return result;
+}
